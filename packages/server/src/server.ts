b423c525df949d55c4bdc543c2092cabe1fe1c2d
Margarita Krutikova <margaretkru@gmail.com>
2020-01-26 13:15:52 +0100

    Cleanup server code

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,72 +4,23 @@ import WebSocket from "ws"
 import bodyParser from "body-parser"
 import cors from "cors"
 
+const API_PORT = 8095
+const WS_PORT = 8999
+
 const app = express()
-
 const server = http.createServer(app)
-
 const wss = new WebSocket.Server({ server })
 
-interface ExtWebSocket extends WebSocket {
-  isAlive?: boolean
-}
-
-wss.on("connection", (ws: ExtWebSocket) => {
-  ws.isAlive = true
-
-  ws.on("message", (message: string) => {
-    console.log("received: %s", message)
-    ws.send(`Hello, you sent -> ${message}`)
-  })
-})
-
-type Color = {
-  r: number
-  g: number
-  b: number
-}
-
-type MatrixPixel = {
-  x: number
-  y: number
-  color: Color
-}
-
-const MATRIX_ROWS = 16
-const MATRIX_COLS = 32
-
-const getCoords = (position: number) => {
-  const y = Math.floor(position / MATRIX_COLS)
-  const x = position - y * MATRIX_COLS
-  return { x, y }
-}
-
-const genColorValue = () => {
-  const r = Math.random()
-  return Math.ceil(r * 255)
-}
-
-const sendPixel = (pixel: MatrixPixel) => {
-  const pixels = [pixel]
-  wss.clients.forEach((ws: ExtWebSocket) => {
-    ws.send(JSON.stringify(pixels))
+const notifyWsClients = (message: string) => {
+  wss.clients.forEach((ws: WebSocket) => {
+    ws.send(message)
   })
 }
 
-//start our server
-server.listen(process.env.PORT || 8999, () => {
+server.listen(WS_PORT, () => {
   const address = server.address()
   const port = (address as WebSocket.AddressInfo).port
   console.log(`Server started on port ${port} :)`)
-})
-
-//---------------------------------------------------
-
-const port = 8095 // default port to listen
-
-// define a route handler for the default home page
-app.get("/", (_, res) => {
-  res.send("Hello world!")
 })
 
 app.use(bodyParser.json()) // to support JSON-encoded bodies
@@ -81,23 +32,12 @@ app.use(
 )
 app.use(cors())
 
-app.post("/pixel", (req, res) => {
-  const pixel: MatrixPixel = {
-    x: parseInt(req.body.x),
-    y: parseInt(req.body.y),
-    color: {
-      r: parseInt(req.body.r),
-      g: parseInt(req.body.g),
-      b: parseInt(req.body.b)
-    }
-  }
-
-  console.log(pixel)
+app.post("/matrix", (req, res) => {
   res.status(200).json({ success: true })
-  sendPixel(pixel)
+  notifyWsClients(JSON.stringify(req.body))
 })
 
 // start the Express server
-app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`)
+app.listen(API_PORT, () => {
+  console.log(`server started at http://localhost:${API_PORT}`)
 })
